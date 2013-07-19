be780ed85e8b82d28bf4aa642aa13b9b5ebdd27c
Mika Lackman <mika.lackman(at)gmail.com>
2013-07-19 08:12:17 +0300

    test refactored

--- tests/test_rendering.py
+++ tests/test_rendering.py
@@ -1,20 +1,23 @@
 import unittest
 import yamf
 import sys
-from os.path import join, pardir
+from os.path import join, pardir, abspath, dirname
 
 sys.path.insert(0, join(pardir))
 from prodcompwebappfw.renderer import Renderer
 
 class TestRenderer(unittest.TestCase):
 
+    def setUp(self):
+        self.renderer = Renderer(join(dirname(abspath(__file__)), 'templates'))
+
     def testRenderingTemplate(self):
-        renderer = Renderer('templates')
-        self.assertEquals('hello template', renderer.render('hello_template.txt'))
+        self.assertEquals('hello template', \
+            self.renderer.render('hello_template.txt'))
 
     def testGivingVariables(self):
-        renderer = Renderer('templates')
-        self.assertEquals('hello jee templates', renderer.render('variables.txt', middle_text='jee'))
+        self.assertEquals('hello jee templates', \
+            self.renderer.render('variables.txt', middle_text='jee'))
 
 
 if __name__ == '__main__':
